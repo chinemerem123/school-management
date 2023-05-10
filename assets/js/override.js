c44ecd737908cf977fe13d3e8db72ec2d5b873e2
Nnaji Christian <54823349+chinemerem123@users.noreply.github.com>
2023-05-10 08:21:26 -0700

    pbs

--- assets/js/override.js
+++ assets/js/override.js
@@ -1,1 +1,23 @@
+//User account page adding new category input box
+//===============================================
 
+let btnAddcate=document.getElementById("add-cate");
+let cateBox=document.getElementById("add-cate-box");
+let cateAdd=document.getElementById("cate-add");
+let cateNewItem=document.getElementById("new-item");
+
+let selectCate = document.getElementById("select-cate");
+var option = document.createElement("option");
+option.text = "Kiwi";
+x.add(option);
+
+btnAddcate.addEventListener("click",()=>{
+    cateBox.style.display="block";
+})
+
+if( cateBox.style.display="block"){
+cateAdd.addEventListener("click",()=>{
+   
+    cateBox.style.display="none";
+})
+}

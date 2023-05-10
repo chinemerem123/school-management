6e7ad6fe252b7e4dea0992a2f57b11b23b76fb7a
Nnaji Christian <54823349+chinemerem123@users.noreply.github.com>
2023-05-10 09:34:46 -0700

    user account add item done

--- assets/js/override.js
+++ assets/js/override.js
@@ -1,3 +1,4 @@
+//===============================================
 //User account page adding new category input box
 //===============================================
 
@@ -5,19 +6,30 @@ let btnAddcate=document.getElementById("add-cate");
 let cateBox=document.getElementById("add-cate-box");
 let cateAdd=document.getElementById("cate-add");
 let cateNewItem=document.getElementById("new-item");
-
 let selectCate = document.getElementById("select-cate");
-var option = document.createElement("option");
-option.text = "Kiwi";
-x.add(option);
+
+window.addEventListener("load", ()=>{
+    cateBox.style.display="none";
+})
+
 
 btnAddcate.addEventListener("click",()=>{
     cateBox.style.display="block";
 })
 
+
 if( cateBox.style.display="block"){
 cateAdd.addEventListener("click",()=>{
-   
+
+    let crateOption = document.createElement("option");
+    crateOption.text = cateNewItem.value;
+    selectCate.add(crateOption);
     cateBox.style.display="none";
+    cateNewItem.value="";
+
 })
 }
+
+//=========================================================
+//User account page adding new category input box ends here
+//=========================================================
